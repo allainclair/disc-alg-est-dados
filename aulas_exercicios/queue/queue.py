00af3d60c307ac69651e0c98cf23621a0618f323
Allainclair Flausino dos Santos <allainclair@battlestar.seebot.com.br>
2019-05-27 13:03:16 -0300

    Aula

---
+++ aulas_exercicios/queue/queue.py
@@ -0,0 +1,13 @@
+# TODO Exercicio: usar listas ligadas para implementar fila com o melhor metodo
+# de insercao para facilitar o metodo "dequeue()".
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        pass
+
+    def dequeue(self):
+        pass
---
+++ aulas_exercicios/stack/stack.py
@@ -0,0 +1,13 @@
+# TODO Exercicio: usar listas ligadas para implementar pilha com o melhor metodo
+# de insercao para facilitar o metodo "pop()".
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
